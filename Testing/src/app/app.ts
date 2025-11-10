75332b6535decf5871411083e294362018d12d64
New Project <swapnildhane98@gmail.com>
2025-11-10 15:42:15 +0530

    Testing with Handling user interface

--- Testing/src/app/app.ts
+++ Testing/src/app/app.ts
@@ -1,5 +1,20 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { first } from 'rxjs';
+
+
+// Signals
+const firstName = signal("Morger");
+console.log(firstName());
+firstName.set('Swapnil');
+console.log(firstName());
+
+//computed 
+const firstNameCapitalized = computed(() => firstName().toUpperCase());
+console.log(firstNameCapitalized());
+firstName.set('Dhane');
+console.log(firstNameCapitalized());
+
 
 @Component({
   selector: 'app-root',
@@ -9,4 +24,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('Testing');
+
+  //Setting dynamic properties and attributes
+  isValidUser = signal(true);
+
+  //Handling user interaction
+  cancelSubscription(){
+    console.log("Subscription cancelled.")
+    return;
+  }
 }
