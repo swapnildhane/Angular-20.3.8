fc6fae25cae9127f218437606e322eb9e031cc98
New Project <swapnildhane98@gmail.com>
2025-11-17 12:09:26 +0530

    Use reactive forms in Angular

--- Employee-Management-UI/src/app/app.ts
+++ Employee-Management-UI/src/app/app.ts
@@ -2,12 +2,13 @@ import { Component, signal } from '@angular/core';
 import { Header } from "./components/header/header";
 import { Footer } from "./components/footer/footer";
 import { EmployeeList } from './components/employee-list/employee-list';
-import { RouterOutlet } from "@angular/router";
+import { RouterModule, RouterOutlet } from "@angular/router";
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header, Footer, EmployeeList, RouterOutlet],
+  imports: [Header, Footer, EmployeeList, RouterOutlet,RouterModule,ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
